c226c5865a41c391302c45ccf7c9428324b57f01
Basant Morsi <basant179@gmail.com>
2023-11-22 15:07:18 +0100

    add new restaurant, show restaurant page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/restaurants", to: "restaurants#index", as: :restaurants
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "/restaurants", to: "restaurants#create"
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
 end
